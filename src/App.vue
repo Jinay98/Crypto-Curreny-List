cdefb0b960928f727c300af4e37f374f518761b8
Jinay Parekh <jinayparekh@cintamedtech>
2021-06-12 15:44:01 +0530

    Called 3rd Party endpoint and implemented Pagination

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card text-center m-3">
+    <h3 class="card-header">Crypto Curreny List</h3>
+    <div class="card" style="width: 60%; margin:0 auto;">
+      <table class="card-table table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Symbol</th>
+            <th scope="col">Price</th>
+            <th scope="col">Price Change</th>
+            <th scope="col">Icon</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in pageOfItems" :key="item.id">
+            <td>{{ item.name }}</td>
+
+            <td>{{ item.symbol }}</td>
+            <td>{{ Math.round(parseFloat(item.price) * 1000) / 1000 }}</td>
+            <td>{{ item.change }}%</td>
+            <td>
+              <img :src="item.iconUrl" style="width: 25%; height: 50px" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="card-footer pb-0 pt-3">
+      <jw-pagination
+        :pageSize=this.noOfEntries
+        :items="exampleItems"
+        @changePage="onChangePage"
+      ></jw-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+const exampleItems = []
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      noOfEntries:10,
+      exampleItems,
+      pageOfItems: [],
+    };
+  },
+  mounted() {
+    var url = "https://api.coinranking.com/v1/public/coins/?limit=100"
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => (this.exampleItems = data.data.coins))
+      .catch((err) => console.log(err.message));
+  },
+  methods: {
+    onChangePage(pageOfItems) {
+      this.pageOfItems = pageOfItems;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
